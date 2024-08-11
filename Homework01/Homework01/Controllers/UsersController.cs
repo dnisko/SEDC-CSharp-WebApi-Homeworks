db2bb3279ff6e451c6f18c4152f06e629866d0bd
dnisko <dnisko@hotmail.com>
2024-08-12 00:33:08 +0200

    Add project files.

---
+++ Homework01/Homework01/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using Homework01.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Homework01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet("GetAll")]
+        public ActionResult GetAll()
+        {
+            return Ok(StaticDb.Users);
+        }
+
+        [HttpGet("GetById/{id:int}")]
+        public ActionResult Get(int id)
+        {
+            if (id < 0 || id > StaticDb.Users.Count)
+            {
+                return BadRequest($"Please enter positive number to {StaticDb.Users.Count}");
+            }
+
+            return Ok(StaticDb.Users[id - 1]);
+        }
+    }
+}
---
+++ Homework01/Homework01/Data/StaticDb.cs
@@ -0,0 +1,14 @@
+﻿namespace Homework01.Data
+{
+    public static class StaticDb
+    {
+        public static List<string> Users = new List<string>
+        {
+            "Mile",
+            "Toso",
+            "Cacko",
+            "Risto",
+            "Blazo"
+        };
+    }
+}
